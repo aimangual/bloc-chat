06c43d5f59d82b2d87b200852d2ce38100b0bdba
Alvaro I. Mangual <amangual@live.com>
2017-01-08 22:46:34 -0600

    Messages viewable in rooms with new styling

--- dist/scripts/controllers/homeCtrl.js
+++ dist/scripts/controllers/homeCtrl.js
@@ -1,10 +1,25 @@
 (function () {
 
-    function HomeCtrl($scope, Room) {
-        this.rooms = Room.all;
+    function HomeCtrl($scope, Room, Message) {
+        rooms = Room.all;
+        
+        rooms.$loaded().then(function(rooms) {
+            var roomMessages = '-K_dQ0WpllPn1sSlp-s_';
+            room = rooms.$getRecord(roomMessages);
+            
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
+        });
+        
+        this.rooms = rooms;
+        
+        this.setCurrentRoom = function(room) {
+            $scope.currentRoomName = room;
+            $scope.messages = Message.getByRoomId($scope.currentRoomName.$id);
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', 'Room', 'Message', HomeCtrl]);
 })();
